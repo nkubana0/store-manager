1f4b273e737131daced1259d26a861f07f3ab51b
nkubanajr <i.shema@alustudent.com>
2024-04-17 11:56:02 +0200

    Added class attributes

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    pay_rate = 0.8
+    all = []
+    def __init__(self, name: str, price: float, quantity=0):
+        #assert statements
+        assert price >= 0, f"Price : {price} is not equal or greater than zero!"
+        assert quantity >= 0, f"Quantity : {quantity} is not equal or greater than zero!"
+        
+        #assign self objects
         self.name = name
         self.price = price
         self.quantity = quantity
 
-item1 = Item("Phone", 100, 5)
+        #append items to all
+        Item.all.append(self)
 
-item2 = Item("Laptop",1000, 3)
+    def calculate_total_price(self):
+        return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
-print(item1.quantity)
-print(item2.quantity)
+    def __repr__(self):
+        return f"Item ('{self.name}', {self.price}, {self.quantity})"
+
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
